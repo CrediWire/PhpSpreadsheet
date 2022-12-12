cb9813487dd2fb53e17fd9436b6d943da0c60ca2
gislainh <hardsongis@gmail.com>
2022-12-12 10:06:00 +0100

    Add back support for array formula on version 1.16.0

--- src/PhpSpreadsheet/Writer/Xlsx/Worksheet.php
+++ src/PhpSpreadsheet/Writer/Xlsx/Worksheet.php
@@ -1129,19 +1129,19 @@ class Worksheet extends WriterPart
             $objWriter->writeAttribute('t', 'b');
         }
         // array values are not yet supported
-        //$attributes = $pCell->getFormulaAttributes();
-        //if (($attributes['t'] ?? null) === 'array') {
-        //    $objWriter->startElement('f');
-        //    $objWriter->writeAttribute('t', 'array');
-        //    $objWriter->writeAttribute('ref', $pCellAddress);
-        //    $objWriter->writeAttribute('aca', '1');
-        //    $objWriter->writeAttribute('ca', '1');
-        //    $objWriter->text(substr($cellValue, 1));
-        //    $objWriter->endElement();
-        //} else {
-        //    $objWriter->writeElement('f', Xlfn::addXlfnStripEquals($cellValue));
-        //}
-        $objWriter->writeElement('f', Xlfn::addXlfnStripEquals($cellValue));
+        $attributes = $pCell->getFormulaAttributes();
+        if (($attributes['t'] ?? null) === 'array') {
+            $objWriter->startElement('f');
+            $objWriter->writeAttribute('t', 'array');
+            $objWriter->writeAttribute('ref', $pCell->getCoordinate());
+            $objWriter->writeAttribute('aca', '1');
+            $objWriter->writeAttribute('ca', '1');
+            $objWriter->text(substr($cellValue, 1));
+            $objWriter->endElement();
+        } else {
+            $objWriter->writeElement('f', Xlfn::addXlfnStripEquals($cellValue));
+        }
+        //$objWriter->writeElement('f', Xlfn::addXlfnStripEquals($cellValue));
         self::writeElementIf(
             $objWriter,
             $this->getParentWriter()->getOffice2003Compatibility() === false,
